d7e1f21f2f5b1d3b89520881c032610ce3805ff3
Kondo Takeo <takeoverjp@gmail.com>
2019-05-06 11:41:09 +0000

    Fix style.

--- Test/test_apis.cpp
+++ Test/test_apis.cpp
@@ -33,14 +33,8 @@ void vAssertCalled(unsigned long ulLine, const char* const pcFileName)
 
 #define DEFAULT_STACK_DEPTH 2048
 
-TEST_GROUP(Scheduler)
-{
-  void setup()
-  {
-  }
-  void teardown()
-  {
-  }
+TEST_GROUP(Scheduler){
+  void setup(){} void teardown(){}
 };
 
 static void
@@ -58,23 +52,20 @@ TEST(Scheduler, CallsOneTaskHandler)
   int param = -1;
   int ret = pdFAIL;
   ret = xTaskCreate(justEndSchedulerTask,
-                    "JustEndSchedulerTask",
-                    DEFAULT_STACK_DEPTH,
-                    &param, 0, &task);
+      "JustEndSchedulerTask",
+      DEFAULT_STACK_DEPTH,
+      &param, 0, &task);
   CHECK_EQUAL(pdPASS, ret);
 
   /* 2. EXEC */
   vTaskStartScheduler();
 
-
   /* 3. CHECK */
   CHECK_EQUAL(0, param);
-
 
   /* 4. CLEANUP */
   vTaskDelete(task);
 }
-
 
 static void
 countUpTask(void* pvParameters)
@@ -89,15 +80,12 @@ countUpTask(void* pvParameters)
 
   taskEXIT_CRITICAL();
 
-  if (count == 2)
-    {
-      vTaskEndScheduler();
-    }
-  else
-    {
-      while (1)
-        taskYIELD();
-    }
+  if (count == 2) {
+    vTaskEndScheduler();
+  } else {
+    while (1)
+      taskYIELD();
+  }
 }
 
 TEST(Scheduler, CallsTwoTaskHandlers)
@@ -107,23 +95,21 @@ TEST(Scheduler, CallsTwoTaskHandlers)
   int count = 0;
   int ret = pdFAIL;
   ret = xTaskCreate(countUpTask,
-                    "countUpTask0",
-                    DEFAULT_STACK_DEPTH,
-                    &count, 0, &tasks[0]);
+      "countUpTask0",
+      DEFAULT_STACK_DEPTH,
+      &count, 0, &tasks[0]);
   CHECK_EQUAL(pdPASS, ret);
   ret = xTaskCreate(countUpTask,
-                    "countUpTask1",
-                    DEFAULT_STACK_DEPTH,
-                    &count, 0, &tasks[1]);
+      "countUpTask1",
+      DEFAULT_STACK_DEPTH,
+      &count, 0, &tasks[1]);
   CHECK_EQUAL(pdPASS, ret);
 
   /* 2. EXEC */
   vTaskStartScheduler();
 
-
   /* 3. CHECK */
   CHECK_EQUAL(2, count);
-
 
   /* 4. CLEANUP */
   vTaskDelete(tasks[0]);
@@ -135,7 +121,7 @@ int main(int argc, char* argv[])
   /* Enable -p option to run tests in a separate process.
      To call pthread_once for each test. */
   int new_argc = argc + 1;
-  char **new_argv = (char **)malloc(sizeof(char*));
+  char** new_argv = (char**)malloc(sizeof(char*));
   for (int i = 0; i < argc; i++)
     new_argv[i] = strdup(argv[i]);
   new_argv[argc] = strdup("-p");
