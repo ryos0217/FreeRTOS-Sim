86ca1a5b6a90a002e5272f421c7c4061b973a1a4
ryos <>
2019-05-20 21:38:42 +0900

    mallocの状態に応じたテストを作成した。

--- test/test_tasks.cpp
+++ test/test_tasks.cpp
@@ -7,7 +7,7 @@ extern "C" {
 #include "stab.h"
 }
 
-TEST_GROUP(ClassName)
+TEST_GROUP(xTasksCreateTest)
 {
   void setup()
   {
@@ -20,7 +20,13 @@ TEST_GROUP(ClassName)
 };
 
 
-TEST(ClassName, TestName)
+TEST(xTasksCreateTest, setPortMallocFaild)
+{
+    resetPortMalloc();
+    CHECK_EQUAL(errCOULD_NOT_ALLOCATE_REQUIRED_MEMORY,xTaskCreate(NULL,"task",10,NULL,0,NULL));
+}
+
+TEST(xTasksCreateTest, setPortMallocSuccess)
 {
     setPortMalloc();
     CHECK_EQUAL(pdPASS,xTaskCreate(NULL,"task",10,NULL,0,NULL));
