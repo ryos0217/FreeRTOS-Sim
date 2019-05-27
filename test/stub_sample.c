b435399df107d8d49f79a0409ab6048b6b766e4c
ryos <>
2019-05-27 22:55:15 +0900

    update stub_sample

--- test/stub_sample.c
+++ test/stub_sample.c
@@ -5,14 +5,11 @@
 void *pvPortMalloc( size_t xWantedSize ){}
 void vPortFree( void *pv ){}
 void vAssertCalled( unsigned long ulLine, const char * const pcFileName ){}
-void vListInitialiseItem( ListItem_t * const pxItem ){}
 portSTACK_TYPE *pxPortInitialiseStack( portSTACK_TYPE *pxTopOfStack, pdTASK_CODE pxCode, void *pvParameters ){}
 void vPortEnterCritical( void ){}
 void vPortAddTaskHandle( void *pxTaskHandle ){}
-void vListInsertEnd( List_t * const pxList, ListItem_t * const pxNewListItem ){}
 void vPortExitCritical( void ){}
 void vPortYield( void ){}
-UBaseType_t uxListRemove( ListItem_t * const pxItemToRemove ){return 0;}
 void vPortForciblyEndThread( void *pxTaskToDelete ){}
 portBASE_TYPE xPortSetInterruptMask( void ){return 0;}
 void vPortClearInterruptMask( portBASE_TYPE xMask ){}
@@ -23,6 +20,4 @@ portBASE_TYPE xPortStartScheduler( void ){return 0;}
 void vApplicationTickHook( void ){}
 void vPortEndScheduler( void ){}
 unsigned long ulPortGetTimerValue( void ){}
-void vListInsert( List_t * const pxList, ListItem_t * const pxNewListItem ){}
 void vApplicationIdleHook( void ){}
-void vListInitialise( List_t * const pxList ){}
