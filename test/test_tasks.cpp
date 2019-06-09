fc11de0110cef97e07e20e50162f087c521074a0
ryos <>
2019-06-09 22:13:36 +0900

    update test

--- test/test_tasks.cpp
+++ test/test_tasks.cpp
@@ -1,27 +1,6 @@
 #include <CppUTest/CommandLineTestRunner.h>
 #include <CppUTest/TestHarness.h>
 
-
-extern "C" {
-
-}
-
-TEST_GROUP(ClassName)
-{
-  void setup()
-  {
-  }
-  void teardown()
-  {
-  }
-};
-
-
-TEST(ClassName, TestName)
-{
-    FAIL("first test");
-}
-
 int
 main (int argc, char *argv[])
 {
